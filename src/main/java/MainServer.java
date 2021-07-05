65330e28921c6158836b1ecfb2d0bf66a48e443e
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-07-04 21:43:26 -0300

    another line

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -1,6 +1,5 @@
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import javax.xml.crypto.Data;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,33 +7,36 @@ public class MainServer {
 	public static void main(String[] args) {
 		try {
 			ServerSocket server = new ServerSocket(8080);
-			
+
 			System.out.println("Esperando conexao...");
 			Socket conexaoCliente = server.accept();
 			System.out.println("CHEGOU CONEXAO !");
-			
+
+
 			InputStream is = conexaoCliente.getInputStream();
 			OutputStream os = conexaoCliente.getOutputStream();
-			
+
 			DataOutputStream dos = new DataOutputStream(os);
-			
-			String resp = "HTTP/1.1 200 OK\nContent-Type:text/html\n\n" + 
-					"<html><title> Aula ISI - </title><h3> Aula ISI</h3></html>";
-			
+			DataInputStream dis =  new DataInputStream(is);
+
+			String resp = "HTTP/1.1 200 OK\r\nContent-Type:text/html\n\n" +
+					"<html><title> Aula ISI - </title><h3> Aula Socket</h3></html>";
+
 			dos.write(resp.getBytes());
-			
-			
+
+
 			Thread.sleep(2000);
 			int x = 0;
 			while((x = is.read())!=-1) {
 				System.out.print((char)x);
-				
-				//os.write(x+1);
+
+				dos.write("foda se".getBytes());
+
+
+				//os.write("power".getBytes());
 			}
-			
-			dos.close();
-			is.close();
-			conexaoCliente.close();
+
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
