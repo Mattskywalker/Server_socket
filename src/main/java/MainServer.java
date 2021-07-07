9a67302ef95038e6c85e6808a068d6fabe2727b3
Vinicius <pedro.vinicius@upe.br>
2021-07-07 19:50:59 -0300

    Versão Final

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -1,4 +1,3 @@
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,6 +6,7 @@ public class MainServer {
 	public static void main(String[] args) {
 		try {
 			ServerSocket server = new ServerSocket(8080);
+
 
 			System.out.println("Esperando conexao...");
 			Socket conexaoCliente = server.accept();
@@ -20,7 +20,41 @@ public class MainServer {
 			DataInputStream dis =  new DataInputStream(is);
 
 			String resp = "HTTP/1.1 200 OK\r\nContent-Type:text/html\n\n" +
-					"<html><title> Aula ISI - </title><h3> Aula Socket</h3></html>";
+					"<!DOCTYPE html>\n" +
+					"<html lang=\"en\">\n" +
+					"<head>\n" +
+					"    <meta charset=\"UTF-8\">\n" +
+					"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+					"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+					"    <title>Document</title>\n" +
+					"</head>\n" +
+					"<body style=\"background-color: grey;\">\n" +
+					"    <script>\n" +
+					"        let d = new Date();\n" +
+					"    \n" +
+					"        function tentar(){\n" +
+					"            if(document.getElementById('time').value ==  \"00\" +\":\"+ d.getSeconds()){\n" +
+					"            document.body.innerHTML = \"<h1>Você acertou, \"+document.getElementById('nome').value+ \" :)\"+\"</h1><br><h1>você chutou:\"+document.getElementById('time').value+\"</h1><br><h1>Time right now is:  \" + d.getHours() + \":\" + d.getMinutes() + \":\" + d.getSeconds()\n" +
+					"        \"</h1>\"\n" +
+					"           \n" +
+					"            \n" +
+					"        }\n" +
+					"        else{\n" +
+					"            document.body.innerHTML = \"<h1>Você Errou, \"+document.getElementById('nome').value+ \" :(\"+ \"</h1><br><h1>você chutou:\"+document.getElementById('time').value+\"</h1><br><h1>Time right now is:  \" + d.getHours() + \":\" + d.getMinutes() + \":\" + d.getSeconds()\n" +
+					"        \"</h1>\" \n" +
+					"        }\n" +
+					"        }\n" +
+					"        \n" +
+					"        </script> \n" +
+					"        <div style=\"display: flex; flex-direction: column; align-items: center; margin-top: 20%; margin-left: 40%; background-color: darkgreen; width: 300px; height: 170px;border-radius: 12px;\">\n" +
+					"            <label for=\"nome\" style=\"font-size: large;\">Insira seu nome:  </label> \n" +
+					"            <input type=\"text\" id=\"nome\" style=\"margin-top: 3%;\"> \n" +
+					"            <br>\n" +
+					"            <label for=\"time\" >Insira quantos segundos você acha que está:  </label> \n" +
+					"            <input type=\"time\" id=\"time\" style=\"margin-top: 3%;\" min=\"00:00\" max=\"00:59\" required>\n" +
+					"                <button type=\"submit\" onclick=\"tentar()\" style=\"margin-top: 3%;\">enviar</button></div>\n" +
+					"</body>\n" +
+					"</html>";
 
 			dos.write(resp.getBytes());
 
@@ -30,7 +64,7 @@ public class MainServer {
 			while((x = is.read())!=-1) {
 				System.out.print((char)x);
 
-				dos.write("foda se".getBytes());
+				dos.write(" ".getBytes());
 
 
 				//os.write("power".getBytes());
